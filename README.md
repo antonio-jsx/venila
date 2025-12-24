c11fc50ae8916fc978ee9fc876833f8410e66fd0
antonio-jsx <antoniodev.1993@gmail.com>
2025-12-23 23:30:34 -0300

    feat: add authentication better-auth

--- README.md
+++ README.md
@@ -4,9 +4,17 @@
 * Internalization(i18n) - next-intl
 * Environment variable - T3Env
 * DataBase - Drizzle-ORM
+* Authentication - Better-Auth
 
 ## Environment Variables
 
 | KEY | DOCS |
 | --- | --- |
 | DATABASE_URL | [PostgreSQL](https://www.postgresql.org/) |
+| BETTER_AUTH_SECRET | use -> openssl rand -base64 32 |
+
+### Better-Auth
+
+``` bash
+npx @better-auth/cli generate --output ./lib/db/schemas/users.ts
+```
--- proxy.ts
+++ proxy.ts
@@ -1,11 +1,28 @@
-import { routing } from './i18n/routing';
+import { routing } from '@/i18n/routing';
+import { getSessionCookie } from 'better-auth/cookies';
+import { type NextRequest, NextResponse } from 'next/server';
 import createMiddleware from 'next-intl/middleware';
 
-export default createMiddleware(routing);
+const handleI18nRouting = createMiddleware(routing);
+
+function stripLocale(pathname: string) {
+  const localePattern = new RegExp(`^/(${routing.locales.join('|')})(/|$)`);
+  return pathname.replace(localePattern, '/');
+}
+
+export default function middleware(request: NextRequest) {
+  const response = handleI18nRouting(request);
+  const pathname = stripLocale(request.nextUrl.pathname);
+
+  if (pathname.startsWith('/admin')) {
+    if (!getSessionCookie(request)) {
+      return NextResponse.redirect(new URL('/signin', request.url));
+    }
+  }
+
+  return response;
+}
 
 export const config = {
-  // Match all pathnames except for
-  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
-  // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)',
+  matcher: ['/((?!api|trpc|_next|_vercel|.*\\..*).*)'],
 };
