9b3b2add24eb0ab26078bbccd15d0e30672af1bf
antonio-jsx <antoniodev.1993@gmail.com>
2025-12-24 12:56:26 -0300

    refactor: user schema

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,6 +1,7 @@
+import * as users from '@/lib/db/schemas/users';
 import { envServer } from '@/lib/env/server';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
 export const client = postgres(envServer.DATABASE_URL);
-export const db = drizzle(client, { schema: {} });
+export const db = drizzle(client, { schema: { ...users } });
