a40c763ede4d24ccb1a15b5aca1d0fc4ce516ec7
antonio-jsx <antoniodev.1993@gmail.com>
2026-01-29 17:12:42 -0300

    feat: add logo icon component

--- app/[locale]/admin/(admin)/_components/brand.tsx
+++ app/[locale]/admin/(admin)/_components/brand.tsx
@@ -1,14 +1,12 @@
 'use client';
 
+import { Logo } from '@/components/logo';
 import { Link } from '@/lib/i18n/navigation';
-import { TicketIcon } from 'lucide-react';
 
 export function Brand() {
   return (
     <Link href="/admin" className="flex items-center gap-1">
-      <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-primary text-sidebar-primary-foreground">
-        <TicketIcon className="size-5" />
-      </div>
+      <Logo className="size-6 fill-black dark:fill-white" />
       <span className="font-medium text-lg">Venila</span>
     </Link>
   );
