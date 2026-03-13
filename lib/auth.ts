7b95cc77b510c5374a5e9266afb0441295a0bf20
antonio-jsx <antoniodev.1993@gmail.com>
2026-03-12 22:13:12 -0300

    refactor: set baseURL

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,5 @@
 import { db } from '@/lib/db';
+import { envClient } from '@/lib/env/client';
 import { ac, admin, moderador } from './permissions';
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
@@ -6,6 +7,7 @@ import { nextCookies } from 'better-auth/next-js';
 import { admin as adminPlugin } from 'better-auth/plugins';
 
 export const auth = betterAuth({
+  baseURL: envClient.NEXT_PUBLIC_APP_URL,
   database: drizzleAdapter(db, {
     provider: 'pg',
     usePlural: true,
--- lib/env/client.ts
+++ lib/env/client.ts
@@ -1,7 +1,12 @@
 import { createEnv } from '@t3-oss/env-nextjs';
+import * as z from 'zod/v4';
 
 export const envClient = createEnv({
-  client: {},
-  runtimeEnv: {},
+  client: {
+    NEXT_PUBLIC_APP_URL: z.url(),
+  },
+  runtimeEnv: {
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+  },
   emptyStringAsUndefined: true,
 });
