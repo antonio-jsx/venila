fc44bc3730f4a0c3d32f56cfaa5af08317f4bd60
antonio-jsx <antoniodev.1993@gmail.com>
2026-03-16 20:50:19 -0300

    feat: auth drizzle adapter

---
+++ lib/auth/index.ts
@@ -0,0 +1,20 @@
+import { ac, admin, moderador } from '@/lib/auth/permissions';
+import { db } from '@/lib/db';
+import { envClient } from '@/lib/env/client';
+import { drizzleAdapter } from '@better-auth/drizzle-adapter';
+import { betterAuth } from 'better-auth';
+import { nextCookies } from 'better-auth/next-js';
+import { admin as adminPlugin } from 'better-auth/plugins';
+
+export const auth = betterAuth({
+  baseURL: envClient.NEXT_PUBLIC_APP_URL,
+  database: drizzleAdapter(db, {
+    provider: 'pg',
+    usePlural: true,
+  }),
+  emailAndPassword: {
+    enabled: true,
+    disableSignUp: true,
+  },
+  plugins: [adminPlugin({ ac, roles: { admin, moderador } }), nextCookies()],
+});
